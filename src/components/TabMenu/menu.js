7a1f08f2f852501ad0035f2d78f722875bae10f4
Ayush Kumar <kumarayush7767@gmail.com>
2021-09-20 21:04:11 +0530

    my first commit

---
+++ src/components/TabMenu/menu.js
@@ -0,0 +1,77 @@
+const Menu=[
+     {
+         id:1,
+         image:"images/maggi.jpg",
+         name:"maggi",
+         category:"breakfast",
+         price:"12$",
+         description:"This is awesome,i like it very much"
+     },
+     {
+        id:2,
+        image:"images/allupakoida.jpg",
+        name:"allu pakoida",
+        category:"evening",
+        price:"20$",
+        description:"This is awesome,i like it very much"
+    },
+    {
+        id:3,
+        image:"images/corn.jpg",
+        name:"corn",
+        category:"breakfast",
+        price:"10$",
+        description:"This is awesome,i like it very much"
+    },
+    {
+        id:4,
+        image:"images/chola.jpg",
+        name:"chola",
+        category:"lunch",
+        price:"50$",
+        description:"This is awesome,i like it very much"
+    },
+    {
+        id:5,
+        image:"images/pizza.jpg",
+        name:"pizza",
+        category:"evening",
+        price:"80$",
+        description:"This is awesome,i like it very much"
+    },
+    {
+        id:6,
+        image:"images/nonvegthali.jpg",
+        name:"Non-Veg Thali",
+        category:"dinner",
+        price:"180$",
+        description:"This is awesome,i like it very much"
+    },
+    {
+        id:7,
+        image:"images/sweet.jpg",
+        name:"Sweets",
+        category:"dinner",
+        price:"60$",
+        description:"This is awesome,i like it very much"
+    },
+    {
+        id:8,
+        image:"images/rajmarice.jpg",
+        name:"Rajma Rice",
+        category:"lunch",
+        price:"60$",
+        description:"This is awesome,i like it very much"
+    },
+    {
+        id:9,
+        image:"images/samosa.jpg",
+        name:"samosa",
+        category:"evening",
+        price:"10$",
+        description:"This is awesome,i like it very much"
+    },
+
+];
+
+export default Menu;
